dd8de3278775997187dbb4bb5a56767d5fd78572
pradeep <pradeeprai0111@gmail.com>
2020-07-19 10:58:54 +0530

    Merging Backend in Single Project

--- onlineshoppingbackend/src/main/java/com/study/onlineshoppingbackend/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.study.onlineshoppingbackend.dao;
-
-import java.util.List;
-
-import com.study.onlineshoppingbackend.dto.Category;
-
-public interface CategoryDAO{
-	List<Category> list();
-}
